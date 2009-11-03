55b65690ae635b90ea2bba894522e9e4a0d87f77
cling2zard <cling2zard@796fc770-c03e-11de-93d8-795327aa6b9b>
2009-11-03 06:55:41 +0000

    test import

---
+++  tjchemical/Login/Ops/OpCheckInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Text;
+using SMBL.Operation;
+using SMBL.Interface.Database;
+namespace SysCom.Ops
+{
+    public class OpCheckInfo:DBOperation
+    {
+        private DataSet logininfods;
+
+        SMBL.Interface.Database.ISql sqlstring;
+
+        public DataSet LoginInfoDs
+        {
+            get { return logininfods; }
+            set { logininfods = value; }
+        }
+        public OpCheckInfo(String DBName,ISql theISql)
+        {
+            sqlstring = theISql;
+            BindDBName = DBName;
+        }
+
+        public override void processDelegate()
+        {
+            LoginInfoDs = myProcessor.Query(sqlstring);
+        }
+    }
+}
---
+++  tjchemical/Login/Sql/sqlChangePassword.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SMBL.Interface.Database;
+namespace SysCom.Sql
+{
+    class sqlChangePassword:ISql
+    {
+        #region ISql 成员
+
+        public string GetSql()
+        {
+            return "UPDATE Account SET Password='" + Password +
+                                         "' WHERE AccountID='" + AccountID + "'";
+        }
+
+        #endregion
+
+        public sqlChangePassword(LoginInfo ToChek)
+        {
+            Password = ToChek.Password;
+            AccountID = ToChek.Username;
+        }
+        private String AccountID;
+        private String Password;
+        
+    }
+}
