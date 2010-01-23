49f6945869c5ebfe251336550be071f4a3b56d2d
cling2zard <cling2zard@796fc770-c03e-11de-93d8-795327aa6b9b>
2010-01-23 07:35:34 +0000

    权限判定

---
+++ trunk/Login/Sql/sqlAdminInfo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SMBL.Interface.Database;
+namespace SysCom.Sql
+{
+    public class sqlAdminInfo : OldtoNewSql
+    {
+        #region ISql 成员
+
+        public override string GetSql()
+        {
+            return "SELECT * FROM Admin WHERE Admin=@Admin";
+        }
+
+        #endregion
+
+
+        public sqlAdminInfo(string Student_ID)
+        {
+            this.key = new Object[] { "@Admin" };
+            this.value = new Object[] { Student_ID.Trim() };
+        }
+    }
+}
--- trunk/TjChemicalWeb/Login/Login.aspx.cs
+++ trunk/TjChemicalWeb/Login/Login.aspx.cs
@@ -31,19 +31,31 @@ public partial class Login_Login : System.Web.UI.Page
             case SysCom.LoginType.Succeed:
                 Session["IdentifyNumber"] = li.Username;
                 Session["LoginPassword"] = li.Password;
+              
+                if (SysCom.Login.CheckAuthority(li))
+                {
+                    Session["Authority"] = "Admin";
+                  
+                  
+                }   
+                else{
+                    Session["Authority"] = "Student";
+                  
+                
+                }
                 pLogin.Visible = false;
                 pSucceed.Visible = true;
-                String toUrl;
+               String toUrl;
                 if (Request.QueryString["toURL"] != null) toUrl = Request.QueryString["toURL"];
                 else
                 {
                     toUrl = "http://" + HttpContext.Current.Request.Url.Authority +HttpContext.Current.Request.ApplicationPath;
                     if (!toUrl.EndsWith("/")) toUrl += "/";
                     toUrl += "Default.aspx";
-                }
+               }
                 hlToUrl.NavigateUrl = toUrl;
-                hlToUrl.Text = toUrl;
-                AutoRedirect("showurl", 3, toUrl);
+              hlToUrl.Text = toUrl;
+             AutoRedirect("showurl", 3, toUrl);
                 break;
             case SysCom.LoginType.PsError:
                 lbErrorMessage.Text = "密码错误";
