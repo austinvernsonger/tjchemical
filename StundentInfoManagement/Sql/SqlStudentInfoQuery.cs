b899a8ef052a73e89d5a2bbeb5cac0404c9e328e
jiasong.song <jiasong.song@gmail.com>
2010-02-03 02:49:46 +0000

    完成教师对学生信息的多条件组合查询

--- StundentInfoManagement/Sql/SqlStudentInfoQuery.cs
+++ StundentInfoManagement/Sql/SqlStudentInfoQuery.cs
@@ -8,15 +8,53 @@ namespace StundentInfoManagement.Sql
     class SqlStudentInfoQuery : OldtoNewSql
     {
         private String returnString;
-        public void GetCondition(String strStudentID, String strName, String strNation, String strDepartment,
-        String strField, String strEntranceYear,String strStudentType, String strGraduationType, String strWorkUnit)
+        public void GetCondition(String strStudentID, String strName, String strNation, Int16 strDepartment,
+        String strField, Int32 strEntranceYear,Int16 strStudentType, Int16 strGraduationType, Int16 strWorkUnit)
         {
             this.key = new object[] {"@StudentID","@Name","@Nation","@Department","@Field","@EntranceYear","@StudentType","@GraduationType","@WorkUnit"};
-            this.value = new object[] {strStudentID.Trim(),strName.Trim(),strNation.Trim(),strDepartment.Trim(),strField.Trim(),strEntranceYear.Trim(),strStudentType.Trim(),strGraduationType.Trim(),strWorkUnit.Trim() };
+            this.value = new object[] {strStudentID.Trim(),strName.Trim(),strNation.Trim(),strDepartment,strField.Trim(),strEntranceYear,strStudentType,strGraduationType,strWorkUnit };
+            returnString = "select StudentID, Name, Nation, DepartmentID,WorkUnitID,Major,EntryYear,TypeofStudent,GraduationType from Student where 1=1";
+            if (strStudentID.Trim() != String.Empty)
+            {
+                returnString += " and StudentID like @StudentID";
+            }
+            if (strName.Trim() != String.Empty)
+            {
+                returnString += " and Name like @Name";
+            }
+            if (strNation.Trim() != String.Empty)
+            {
+                returnString += " and Nation like @Nation";
+            }
+            if (strDepartment != -1)
+            {
+                returnString += " and DepartmentID = @Department";
+            }
+            if (strField != String.Empty)
+            {
+                returnString += " and Major like @Field";
+            }
+            if (strEntranceYear != 0)
+            {
+                returnString += " and EntryYear = @EntranceYear";
+            }
+            if (strStudentType != -1)
+            {
+                returnString += " and TypeofStudent = @StudentType";
+            }
+            if (strGraduationType != -1)
+            {
+                returnString += " and GraduationType = @GraduationType";
+            }
+            if (strWorkUnit != -1)
+            {
+                returnString += " and WorkUnitID = @WorkUnit";
+            }
         }
         public override string GetSql()
         {
-            return "select StudentID, Name, Nation, DepartmentID,WorkUnitID,Major,EntryYear,TypeofStudent,GraduationType from Student where StudentID like @StudentID and Name like @Nmae and Nation like @Nation and DepartmentID like @Department and Major like @Field and EntryYear like @EntranceYear and TypeofStudent like @StudentType and GraduationType like @Graduation and WorkUnitID like @WorkUnitID";
+            
+            return returnString;
         }
     }
 }
